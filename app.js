f5bc2e18b680cb870d37503b15a538dacae39d1f
sshangs <943101718@qq.com>
2021-02-09 09:28:05 +0800

    测试上传

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-    // 登录,mac上传测试
+    // 登录,mac上传测试,2.9
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
